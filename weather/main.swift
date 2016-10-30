7a33dc7972bd85bb7428b7996844e0b243e123cf
Terry Johnson <giovanni7304@me.com>
2016-10-30 11:39:11 -0500

    Initial Commit

---
+++ weather/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  weather
+//
+//  Created by Terry Johnson on 10/30/16.
+//  Copyright © 2016 Terry Johnson. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
